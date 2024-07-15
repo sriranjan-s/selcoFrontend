97dfebebe95c819e1fd24a1fc3d2a46aa9dd6024
Puli-shilpa <puli.shilpa@pwc.com>
2024-07-15 12:21:34 +0530

    inbox api changes

--- micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/useInboxGeneral/newFilterFn.js
+++ micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/useInboxGeneral/newFilterFn.js
@@ -1,30 +1,23 @@
 export const filterFunctions = {
-  PT: (filtersArg) => {
+  Incident: (filtersArg) => {
     let { uuid } = Digit.UserService.getUser()?.info || {};
 
     const searchFilters = {};
     const workflowFilters = {};
 
-    const { propertyIds, mobileNumber, limit, offset, sortBy, sortOrder, total, applicationStatus, services } = filtersArg || {};
+    const { incidentId, mobileNumber, limit, offset, sortBy, sortOrder, total, applicationStatus, services } = filtersArg || {};
 
-    if (filtersArg?.acknowledgementIds) {
-      searchFilters.applicationNumber = filtersArg?.acknowledgementIds;
+    if (filtersArg?.IncidentWrappers) {
+      searchFilters.applicationNumber = filtersArg?.incidentId;
     }
-    if (filtersArg?.propertyIds) {
-      searchFilters.propertyId = propertyIds;
-    }
-    if (filtersArg?.oldpropertyids) {
-      searchFilters.oldpropertyids = filtersArg?.oldpropertyids;
-    }
+    
     if (applicationStatus && applicationStatus?.[0]) {
       workflowFilters.status = applicationStatus.map((status) => status.uuid);
       if (applicationStatus?.some((e) => e.nonActionableRole)) {
         searchFilters.fetchNonActionableRecords = true;
       }
     }
-    if (filtersArg?.locality?.length) {
-      searchFilters.locality = filtersArg?.locality.map((item) => item.code.split("_").pop());
-    }
+    
     if (filtersArg?.uuid && filtersArg?.uuid.code === "ASSIGNED_TO_ME") {
       workflowFilters.assignee = uuid;
     }
@@ -34,9 +27,10 @@ export const filterFunctions = {
     if (services) {
       workflowFilters.businessService = services;
     }
-    searchFilters["isInboxSearch"] = true;
-    searchFilters["creationReason"] = ["CREATE", "MUTATION", "UPDATE"];
-    workflowFilters["moduleName"] = "PT";
+    searchFilters["tenantId"] = Digit.ULBService.getCurrentTenantId();
+   // searchFilters["creationReason"] = ["CREATE", "MUTATION", "UPDATE"];
+    workflowFilters["moduleName"] = "Incident";
+    workflowFilters["tenantId"]=Digit.ULBService.getCurrentTenantId();
 
     // if (limit) {
     //   searchFilters.limit = limit;
